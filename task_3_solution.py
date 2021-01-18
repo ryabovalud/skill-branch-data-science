b4a96533681d40a9028dd5acf775cce25ec8f709
ryabovalud <43740076+ryabovalud@users.noreply.github.com>
2021-01-18 16:23:58 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,11 +1,5 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from copy import deepcopy
-from tqdm import tqdm
 
 from sklearn.datasets import make_regression
 from sklearn.linear_model import SGDClassifier
