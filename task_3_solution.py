8a14a2752b15472395b75a27f5f6c98914e7a02e
ryabovalud <43740076+ryabovalud@users.noreply.github.com>
2021-01-18 18:49:29 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,71 +1,100 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.datasets import make_regression
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-# Задание 1
-def split_data_into_two_samples(df):
-    return train_test_split(df, test_size=0.3, random_state=42)
+# Задание 1.
+# Для начала, попробуем разбить данные на обучающую часть и валидационную часть в соотношении 70 / 30 и
+# предварительным перемешиванием. Параметр `random_state` зафиксировать 42.
+# Назовите функцию `split_data_into_two_samples`, которая принимает полный датафрейм,
+# а возвращает 2 датафрейма: для обучения и для валидации.
+def split_data_into_two_samples(x):
+    x_train, x_test = train_test_split(x, test_size=0.3, random_state=42)
+    return [x_train, x_test]
 
-# Задание 2
-def prepare_data(df):
-    objects = df.select_dtypes(include=['object']).dtypes
-    y = df["price_doc"]
-    X = df.drop(columns=["price_doc"])
-    X = X.drop(columns=objects.index)
-    X = X.dropna()
-    return X, y
+
+# Задание 2.
+# продолжим выполнение предварительной подготовки данных: в данных много категориальных признаков
+# (они представлены типами `object`), пока мы с ними работать не умеем, поэтому удалим их из датафрейма.
+# Кроме того, для обучения нам не нужна целевая переменная, ее нужно выделить в отдельный вектор (`price_doc`).
+# Написать функцию `prepare_data`, которая принимает датафрейм, удаляет категориальные признаки, удаляет `id`,
+# и выделяет целевую переменную в отдельный вектор.
+# Кроме того, некоторые признаки содержат пропуски, требуется удалить такие признаки.
+# Функция должна возвращать подготовленную матрицу признаков и вектор с целевой переменной.
+def prepare_data(x):
+    target_vector = x['price_doc']
+    objects = x.select_dtypes(include=['object']).columns
+    filtered = x.drop(columns=objects).drop(columns=["id"]).drop(columns=['price_doc']).dropna(axis=1)
+    return [filtered, target_vector]
+
 
 # Задание 3
-def scale(df, scaler):
-    df_scaled = scaler.fit_transform(df)
-    return df_scaled
+# Перед обучением линейной модели также необходимо масштабировать признаки.
+# Для этого мы можем использовать `MinMaxScaler` или `StandardScaler`.
+# Написать функцию, которая принимает на вход датафрейм и трансформем,
+# а возвращает датафрейм с отмасштабированными признаками.
+def scale_data(x, transformer):
+    return transformer.fit_transform(x)
+
 
 # Задание 4
-def prepare_data_for_model(df, scaler):
-    objects = df.select_dtypes(include=['object']).dtypes
-    y = df["price_doc"]
-    X = df.drop(columns=["price_doc"])
-    X = X.drop(columns=objects.index)
-    X = X.dropna()
-    df_scaled = scaler.fit_transform(X)
-    return df_scaled, y
+# объединить задание 2 и 3 в единую функцию `prepare_data_for_model`,
+# функция принимает датафрейм и трансформер для масштабирования,
+# возвращает данные в формате задания 3 и вектор целевой переменной.
+def prepare_data_for_model(x, transformer):
+    x_train, x_test = prepare_data(x)
+    x_scaled_array = scale_data(x_train, transformer)
+    x_train_scaled = pd.DataFrame(x_scaled_array, columns=x_train.columns)
+    return [x_train_scaled, x_test]
 
-# Задание 5
+
+# Задание 5.
+# разбить данные на обучение / валидацию в соответствии с заданием 1.
+# Обучить линейную модель (`LinearRegression`) на данных из обучения.
+# При подготовке данных использовать функцию из задания 4,
+# в качестве трансформера для преобразования данных использовать - `StandardScaler`.
+# Создать функцию `fit_first_linear_model`, которая принимает на вход `x_train` и `y_train`, а возвращает модельку.
 def fit_first_linear_model(x_train, y_train):
+    x_train_scaled = scale_data(x_train, StandardScaler())
     model = LinearRegression()
-    model.fit(x_train, y_train)
+    model.fit(x_train_scaled, y_train)
     return model
 
-# Задание 6
 
-    
+# Задание 6.
+# выполнить задание 5, но с использованием `MinMaxScaler`.
+def fit_first_linear_model_2(x_train, y_train):
+    x_train_scaled = scale_data(x_train, MinMaxScaler())
+    model = LinearRegression()
+    model.fit(x_train_scaled, y_train)
+    return model
+
+
 # Задание 7
-def evaluate_model(model, X_train, y_train):
-    y_pred = model.predict(X_train[numeric_features])
-    MSE = round(np.sqrt(mean_squared_error(y_train, y_pred)),2)
-    MAE = round(mean_absolute_error(y_train, y_pred),2)
-    R2 =  round(r2_score(y_train, y_pred),2)
-    return MSE, MAE, R2
+# написать функцию для оценки качества модели - `evaluate_model`, которая принимает на вход обученную модель,
+# выборку для построения прогнозов и вектор истинных ответов для выборки. Внутри функции вычислить метрики `MSE`,
+# `MAE`, `R2`, вернуть значения метрик, округленных до 2-го знака.
+# Для построения / оценки качества использовать разбиение из задания 1
+def evaluate_model(linreg, x_test, y_test):
+    y_pred = linreg.predict(x_test)
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    return [round(mse, 2), round(mae, 2), round(r2, 2)]
 
 
-# Задание 9
-# написать функцию `calculate_squared_stats_by_material`, которая принимает на вход датафрейм `X` и возвращает максимальную 
-# и минимальную площадь квартир в зависимости от материала изготовления дома. Каждое значение площади округлить до 2-го знака.
-def calculate_squared_stats_by_material(x):
-    pivot_table = pd.pivot_table(x, index=['material'], values='full_sq', aggfunc={'full_sq': [np.max, np.min]})
-    return round(pivot_table, 2)
-
-# Задание 10
-# написать функцию `calculate_crosstab`, которая принимает на вход датафрейм X и возвращает максимальную и минимальную стоимость 
-# квартир в зависимости от района города и цели покупки. 
-# Ответ - сводная таблица, где индекс - район города (признак - `sub_area`),
-# столбцы - цель покупки (признак - `product_type`). Каждое значение цены округлить до 2-го знака, пропуски заполнить нулем.
-def calculate_crosstab(x):
-    cross_table = pd.crosstab(index=x['sub_area'], columns=x['product_type'], values=x['price_doc'], aggfunc=np.mean).fillna(0)
-    return round(cross_table, 2)
+# Задание 8
+# написать функцию, которая принимает на вход обученную модель и список названий признаков,
+# и создает датафрейм с названием признака и абсолютным значением веса признака.
+# Датафрейм отсортировать по убыванию важности признаков и вернуть.
+# Назвать функцию `calculate_model_weights`. Для удобства, колонки датафрейма назвать `features` и `weights`.
+def calculate_model_weights(linreg, names):
+    df = pd.DataFrame({
+        'features': names,
+        'weights': linreg.coef_
+    })
+    df.sort_values(by=['weights'])
+    return df
